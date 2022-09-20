e04c3adc117f3ab22ebed4df0e48727b67ed1902
Stoyan Stoyanov <95132005+Stoyan83@users.noreply.github.com>
2022-09-20 16:19:30 +0300

    Update 02_open_closed.rb

--- 02_open_closed.rb
+++ 02_open_closed.rb
@@ -12,7 +12,7 @@ class StudenTaxes
   end
 
   def get_discount
-    if @average_grade > 5
+    if @average_grade > 5.3
       @semester_taxes * 0.7
       # elsif @average_grade > 4.5
       #     @semester_taxes * 0.90
